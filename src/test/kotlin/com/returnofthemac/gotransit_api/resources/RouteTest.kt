d9e803c7d614f1993aff03ea534587160c8f813e
Matt MacAulay <mac@twg.ca>
2016-05-25 09:07:47 -0400

    Added route serialization test

---
+++ src/test/kotlin/com/returnofthemac/gotransit_api/resources/RouteTest.kt
@@ -0,0 +1,25 @@
+package com.returnofthemac.gotransit_api.resources
+
+import com.returnofthemac.gotransit_api.gtfs.Route
+import io.dropwizard.jackson.Jackson
+import io.dropwizard.testing.FixtureHelpers.fixture
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class RouteTest {
+    val mapper = Jackson.newObjectMapper()
+    @Test
+    fun testRouteSerialization() {
+        val route = Route(
+                id = "237-ST",
+                agencyId = "GO",
+                colour = "794500",
+                shortName = "ST",
+                longName = "Stouffville",
+                textColour = "FFFFFF",
+                type = 2)
+        val expected = mapper.writeValueAsString(mapper.readValue(fixture("fixtures/route.json"), Route::class.java))
+        val actual = mapper.writeValueAsString(route)
+        assertEquals(expected, actual)
+    }
+}
